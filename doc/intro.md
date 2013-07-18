2cceee561dbc3c1a3648de04742bab0322f923ca
Kaylen Wheeler <kfjwheeler@gmail.com>
2013-07-18 13:29:07 -0700

    First commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to complecs
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/complecs/core_test.clj
@@ -0,0 +1,7 @@
+(ns complecs.core-test
+  (:use clojure.test
+        complecs.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
